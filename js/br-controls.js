a5840dcdbe934dbc960460ef70b34505139b851c
Brian J Ridgeway <Brian.J.Ridgeway@census.gov>
2016-05-09 09:32:04 -0400

    use initialization for off selected values -- if eval string exists wait until it's true before setting not-selected values

--- js/br-controls.js
+++ js/br-controls.js
@@ -12,18 +12,29 @@ angular.module( 'br.controls', [] )
     },
     template: '<input type="radio" ng-model="model" ng-change="check()" name="{{name}}" id="{{id}}" ng-value="value">',
     link: function( scope ,element, attributes ){
-      if( scope.initiate && eval(scope.initiate )){
-        if( !element[0].firstChild.checked ){
-          scope.model = scope.offValue;
-        }
+      if( scope.initiate === undefined ){
+        scope.initiated = true;
       }
-      scope.$watch( "isChecked()", function(newStatus){
-       if( newStatus ){
-          scope.model = scope.value
+      scope.isInitiated = function(){
+        return ( scope.initiate === undefined || eval(scope.initiate ))
+      }
+      scope.$watch('isInitiated()', function( active ){
+        if( !scope.initiated && active ){
+          scope.initiated = true
+          if( !element[0].firstChild.checked ){
+            scope.model = scope.offValue;
+          }
         }
-        else{
-          scope.model = scope.offValue
-        } 
+      })
+      scope.$watch( "isChecked()", function(newStatus){
+        if( scope.initiated ){
+          if( newStatus ){
+            scope.model = scope.value
+          }
+          else{
+            scope.model = scope.offValue
+          } 
+        }
       })
       scope.isChecked = function(){
         return element[0].firstChild.checked
