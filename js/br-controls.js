dde5c5086db3cd29fcb51cd9082ae941265819ab
Brian J Ridgeway <Brian.J.Ridgeway@census.gov>
2016-05-10 08:12:34 -0400

    changing directive to a class, let's reuse most of the radio input insetad of rewriting, adding checkboxes

--- js/br-controls.js
+++ js/br-controls.js
@@ -1,16 +1,11 @@
 angular.module( 'br.controls', [] )
 .directive( 'brRadio', function(){
   return {
-    restrict: 'E',
+    restrict: 'C',
     scope: {
-      name: '@',
-      id: '@',
-      value: '@',
-      offValue: '@',
-      model: "=",
+      ngModel: '=',
       initiate: "="
     },
-    template: '<input type="radio" ng-model="model" name="{{name}}" id="{{id}}" ng-value="value">',
     link: function( scope ,element, attributes ){
       if( scope.initiate === undefined ){
         scope.initiated = true;
@@ -21,24 +16,50 @@ angular.module( 'br.controls', [] )
       scope.$watch('isInitiated()', function( active ){
         if( !scope.initiated && active ){
           scope.initiated = true
-          if( !element[0].firstChild.checked ){
-            scope.model = scope.offValue;
+          if( !element[0].checked ){
+            scope.ngModel = attributes.brFalseValue;
           }
         }
       })
       scope.$watch( "isChecked()", function(newStatus){
         if( scope.initiated ){
           if( newStatus ){
-            scope.model = scope.value
+            scope.ngModel = attributes.value
           }
           else{
-            scope.model = scope.offValue
-          } 
+            scope.ngModel = attributes.brFalseValue
+          }
         }
       })
       scope.isChecked = function(){
-        return element[0].firstChild.checked
+        return element[0].checked
       }
     }
   }
 })
+.directive( 'brCheckbox', function(){
+  return {
+    restrict: 'C',
+    scope: {
+      ngModel: '=',
+      initiate: "="
+    },
+    link: function( scope ,element, attributes ){
+      if( scope.initiate === undefined ){
+        scope.initiated = true;
+      }
+      scope.isInitiated = function(){
+        return ( scope.initiate === undefined || eval(scope.initiate ))
+      }
+      scope.$watch('isInitiated()', function( active ){
+        if( !scope.initiated && active ){
+          scope.initiated = true
+          if( !element[0].checked ){
+            scope.ngModel = attributes.ngFalseValue;
+          }
+        }
+      })
+    }
+  }
+})
+
